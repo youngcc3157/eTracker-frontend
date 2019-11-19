b4acfb58b0a1a03b9e913bcbfa60b07a545680a3
Mario F <formim@rpi.edu>
2019-11-19 18:55:04 -0500

    Tabs

--- gui/src/App.js
+++ gui/src/App.js
@@ -4,15 +4,36 @@ import BaseRouter from './routes';
 import 'antd/dist/antd.css';
 
 import CustomLayout from './containers/Layout';
+import Tabs from './Tabs';
+require('./App.css');
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <CustomLayout>
-          <BaseRouter />
-        </CustomLayout>
-      </Router>
+    <Router>
+      <CustomLayout>
+        <BaseRouter />
+      </CustomLayout>
+    </Router>
+
+    <h1> eTracker </h1>
+      <Tabs>
+        <div label="Emails">
+
+        </div>
+        <div label="Applied">
+
+        </div>
+        <div label="Interviews">
+
+        </div>
+        <div label="On-Site">
+
+        </div>
+        <div label="Offers">
+
+        </div>
+      </Tabs>
     </div>
   );
 }
